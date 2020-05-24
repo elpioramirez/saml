acbd890b49a20b2a6d2b9dfe6bbe17a50e2ac010
Hector Ramirez <elpioramirez@gmail.com>
2020-05-24 12:24:10 -0500

    BASE

---
+++ src/test/java/com/gaval/saml/SamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaval.saml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
